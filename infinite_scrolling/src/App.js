8a080155b84b1cbeaae0ffd2822ea4a4ae58c36f
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-05 11:21:31 +0530

    routing-done

--- infinite_scrolling/src/App.js
+++ infinite_scrolling/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { Login } from './Pages/Login';
 
 function App() {
   return (
     <div className="App">
-        
+         <Routes>
+                <Route  path='/home' element={<Home/>}/>
+                <Route  path='/' element={<Login/>}/>
+         </Routes>
     </div>
   );
 }
